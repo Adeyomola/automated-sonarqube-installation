396945c6a8672500c20aa1a1c21b0a72b5a2fd53
Adeyomola Kazeem <44479277+Adeyomola@users.noreply.github.com>
2023-10-23 05:04:53 +0100

    Create sonar.sh

---
+++ sonar.sh
@@ -0,0 +1,126 @@
+#! /bin/bash
+
+# add postgreSQL GPG key
+curl https://www.postgresql.org/media/keys/ACCC4CF8.asc | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/apt.postgresql.org.gpg >/dev/null
+
+# add postgreSQL repo
+sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main" >> /etc/apt/sources.list.d/pgdg.list'
+
+# update apt 
+sudo apt update
+
+# install postgreSQL
+sudo apt install postgresql postgresql-contrib -y
+
+# create sonarqube user and database
+sudo -u postgres psql -c "create role sonarqube createdb createrole login password 'adeyomola'"
+sudo -u postgres createdb -O sonarqube sq >> /dev/null
+
+# download sonarqube
+wget https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-9.6.1.59531.zip
+
+# unzip the package
+unzip -q sonarqube-9.6.1.59531.zip
+
+# move sonarqube to the /opt directory
+sudo mv sonarqube-9.6.1.59531 /opt/sonarqube
+
+# delete zip file
+rm sonarqube-9.6.1.59531.zip
+
+# create sonarqube user
+sudo adduser --system --no-create-home --group --disabled-login sonarqube
+
+# grant permissions to /opt/sonarqube
+sudo chown -R sonarqube:sonarqube /opt/sonarqube
+
+# configure the server
+sudo bash -c 'cat << FOE >> /opt/sonarqube/conf/sonar.properties
+sonar.jdbc.username=adeyomola
+sonar.jdbc.password=adeyomola
+sonar.jdbc.url=jdbc:postgresql://localhost:5432/sonarqube
+sonar.web.host=127.0.0.1
+sonar.web.javaAdditionalOpts=-server
+FOE'
+
+# increase memory map
+sudo bash -c 'echo -e "vm.max_map_count=262144\nfs.file-max=65536" >> /etc/sysctl.conf'
+
+# create ulimit file for sonarqube
+sudo touch /etc/security/limits.d/99-sonarqube.conf
+
+# add limits to the ulimit file
+sudo bash -c 'echo -e "sonarqube   -   nofile   65536\nsonarqube   -   nproc    4096" >> /etc/security/limits.d/99-sonarqube.conf'
+
+# create sonarqube service file
+sudo touch /etc/systemd/system/sonarqube.service
+
+# add service configuration
+sudo bash -c 'cat << EOF > /etc/systemd/system/sonarqube.service
+[Unit]
+Description=SonarQube service
+After=syslog.target network.target
+
+[Service]
+Type=forking
+
+ExecStart=/opt/sonarqube/bin/linux-x86-64/sonar.sh start
+ExecStop=/opt/sonarqube/bin/linux-x86-64/sonar.sh stop
+
+User=sonarqube
+Group=sonarqube
+Restart=always
+
+LimitNOFILE=65536
+LimitNPROC=4096
+
+[Install]
+WantedBy=multi-user.target
+EOF'
+
+# start sonarqube service
+sudo systemctl start sonarqube
+
+# enable service on startup
+sudo systemctl enable sonarqube
+
+# show status
+sudo systemctl status sonarqube
+
+## set up reverse proxy with nginx
+# install nginx
+sudo apt install nginx -y
+
+# enable on startup
+sudo systemctl enable nginx
+
+# create nginx config file for sonarqube
+sudo touch /etc/nginx/sites-available/sonarqube.conf
+
+# append configuration for sonarqube
+sudo bash -c 'cat << "EOF" > /etc/nginx/sites-available/sonarqube.conf
+server {
+
+    listen 9000;
+    access_log /var/log/nginx/sonar.access.log;
+    error_log /var/log/nginx/sonar.error.log;
+    proxy_buffers 16 64k;
+    proxy_buffer_size 128k;
+
+    location / {
+        proxy_pass http://127.0.0.1:9000;
+        proxy_next_upstream error timeout invalid_header http_500 http_502 http_503 http_504;
+        proxy_redirect off;
+        proxy_set_header Host $host;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto http;
+    }
+}
+EOF'
+
+# enable the nginx sonarqube config
+sudo ln -s /etc/nginx/sites-available/sonarqube.conf /etc/nginx/sites-enabled/
+
+# restart nginx
+sudo systemctl restart nginx
